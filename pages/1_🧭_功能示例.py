5b44c4ad0e6e3462335072359a16ed0ae9a55648
HAmazon59 <102913800+HAmazon59@users.noreply.github.com>
2022-07-04 15:19:28 +0800

    Add files via upload

---
+++ pages/1_🧭_功能示例.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(page_title="功能示例", page_icon='static_data/变化检测.png')
+
+
+
+
+with st.sidebar:
+    st.markdown("[访问 AI Studio 官网](https://aistudio.baidu.com/aistudio/index '飞浆AI Studio')")
+    st.markdown("[访问 飞浆 官网](https://www.paddlepaddle.org.cn/ '飞浆PaddlePaddle')")
---
+++ pages/2_🌐_功能介绍.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(page_title="功能介绍", page_icon='static_data/变化检测.png')
+
+st.image('static_data/bhjc.png')
